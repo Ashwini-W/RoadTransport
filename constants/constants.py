4a79486d6235d74d135f3723ddbfd9a457da063a
Ashwini <68464195+Ashwini-W@users.noreply.github.com>
2024-11-24 23:27:13 +0530

    Add files via upload

---
+++ constants/constants.py
@@ -0,0 +1,24 @@
+
+
+year=[2018,2019,2020]
+
+state_list=[]
+
+vehicle_types = [
+    'Multiaxled/Articulated Vehicles (I)',
+    'Trucks and Lorries (II)',
+    'Light Motor Vehicles (Goods) (II) - Four Wheelers',
+    'Light Motor Vehicles (Goods) (II) - Three Wheelers',
+    'Buses (IV) - Stage carriages',
+    'Buses (IV) - Contract carriages',
+    'Buses (IV) - Private service vehicles',
+    'Buses (IV) - Other buses',
+    'Taxis (V) - Motor cabs',
+    'Taxis (V) - Maxi cabs',
+    'Taxis (V) - Other taxis',
+    'Light Motor Vehicles (Passengers) (VI) - Three seaters',
+    'Light Motor Vehicles (Passengers) (VI) - Four to six seaters',
+    'Motor cycles on hire (VII)',
+    'Other vehicles not included in (I-VII)',
+    'Total Transport (I TO VII)'
+]
